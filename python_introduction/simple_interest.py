e85593ae5032306a71214d4358554a1db16cce57
Patricia Dadzie <pdadzie502@gmail.com>
2025-07-11 11:54:48 +0000

    simple interest calculations

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,10 @@
+# Simple Interest Calculations in python
+
+principal = 1000
+rate = 0.05
+time = 3
+
+# Formula for Simple Interest 
+simple_interest= principal * rate * time
+
+print(f"Interest rate on ${principal} for {time} years is ${simple_interest}")
