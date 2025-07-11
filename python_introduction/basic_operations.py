f38dc66f133344b343a84d4cd337a4e1aee75401
Patricia Dadzie <pdadzie502@gmail.com>
2025-07-11 11:16:37 +0000

    Basic arithmetic operations in python

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,14 @@
+# Basic arithmetic operations in python
+
+number1 = 10
+number2 = 5
+
+# Calculation of Sum, Difference and Product of number1 and number 2
+sum = number1 + number2
+difference = number1 - number2
+product = number1 * number2
+
+print(f"The sum of number1 and number2 is {sum}")
+print(f"The difference of number1 and number2 is {difference}")
+print(f"The product of number1 and number2 is {product}")
+
