fd931d46c986d44f06596b621fdea7fa31c806b7
Patricia Dadzie <pdadzie502@gmail.com>
2025-07-11 12:41:02 +0000

    Added conversion of hours into seconds

---
+++ python_introduction/hours_to_seconds.py
@@ -0,0 +1,11 @@
+# Conversion of hours to seconds 
+
+# Delaring variables
+hours = 2
+
+# 60 minutes = 1 hour ; 1 minute = 60 seconds
+# Therefore 1 hour = 60 * 60 seconds
+# And 2 hours =2(60*60)seconds
+
+seconds = 2 * (60*60)
+print(f"{hours}hours is {seconds} seconds")
