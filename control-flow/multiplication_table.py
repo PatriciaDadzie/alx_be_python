4459469281aa09421fabd10ba8051d643293ff01
Patricia Dadzie <pdadzie502@gmail.com>
2025-07-17 06:24:22 +0000

    Add multiplication tanle script

---
+++ control-flow/multiplication_table.py
@@ -0,0 +1,9 @@
+# Multiplication Table Generator
+
+# Ask the user to input a number for which they want to see the multiplication table
+number = int(input("Enter a number to see its multiplication table: "))
+
+# iterate from i = 1 to i = 10
+for i in range(1, 11):
+    result = i * number 
+    print(f"{i} * {number} = {result}")
