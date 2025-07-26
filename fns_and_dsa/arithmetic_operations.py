6dab7e74e60bfab38bdf6c99b6675ecf1c29c138
Patricia Dadzie <pdadzie502@gmail.com>
2025-07-26 12:45:02 +0000

    Added arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -12,9 +12,8 @@ def perform_operation(num1, num2, operation):
         return num1 * num2
     
     elif operation == "divide":
-
-        if num2 != 0:
-            return num1 / num2
+        if num2 == 0:
+          return "Cannot divide by zero"  
         else:
-            return "Cannot divide by zero"
+            return num1 / num2
     
