87997e6a058ca55e7ffd9851513e20b389bdefb7
Patricia Dadzie <pdadzie502@gmail.com>
2025-07-26 12:17:13 +0000

    Update arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,24 +1,15 @@
 
 # Declaration of a function that performs basic arithmetic
-
-
-def perform_operation(num1 , num2 , operation):
-
-       match operation:
+def perform_operation(num1, num2, operation):
+    match operation:
         case "add":
             return num1 + num2
-
         case "subtract":
-            return num1 - num2    
-
+            return num1 - num2
         case "multiply":
             return num1 * num2
-
-        
-        case "divide": 
-         # If else to handle num2 = 0
-         if num2 != 0:
-            return num1 / num2
-           
-         else:
-           return "Cannot divide by zero."
+        case "divide":
+            if num2 != 0:
+                return num1 / num2
+            else:
+                return "Error: Cannot divide by zero"
