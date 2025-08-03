b99aba73b884f06a0c535ae52551efff8b88caf5
Patricia Dadzie <pdadzie502@gmail.com>
2025-08-03 15:36:11 +0000

    Simple bank_account.py script

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -16,9 +16,8 @@ class BankAccount:
             self.account_balance -= amount
             return True
         else:
-            print("Insufficient funds.")
             return False
 
     # print the current balance         
     def display_balance(self):
-        print(f"Current Balance: ${self.account_balance:}")
+        print(f"Current Balance: ${self.account_balance:.2f}")
