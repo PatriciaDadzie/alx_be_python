6f93f7577f48aa05a935e2a25af24a93c35d1e51
Patricia Dadzie <pdadzie502@gmail.com>
2025-08-07 12:00:58 +0000

    class_static_methods_demo.py script

---
+++ oop/class_static_methods_demo.py
@@ -0,0 +1,15 @@
+# Calculator class
+class Calculator:
+    # Class attribute
+    calculation_type = "Arithmetic Operations"
+
+    # Static method
+    @staticmethod
+    def add(a, b):
+        return a + b
+
+    # Class method (uses class-level data:'cls')
+    @classmethod
+    def multiply(cls, a, b):
+        print(f"Calculation type: {cls.calculation_type}")
+        return a * b
